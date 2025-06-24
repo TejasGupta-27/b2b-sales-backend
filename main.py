697359b373fe68024b7c4698d67e90810c219223
TejasGupta-27 <b22cs093@iitj.ac.in>
2025-06-24 12:43:47 +0530

    New Relic Removal

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# New Relic monitoring setup
-import os
-if os.getenv("NEWRELIC_ENABLED", "True").lower() == "true":
-    import newrelic.agent
-    newrelic.agent.initialize('newrelic.ini')
-
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
